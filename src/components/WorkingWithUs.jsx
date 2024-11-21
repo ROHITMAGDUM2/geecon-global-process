bf72d8d5184f075ca8f8f75845c3a1b767df410d
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2024-11-21 18:54:44 +0530

    workingwithus 01

--- src/components/WorkingWithUs.jsx
+++ src/components/WorkingWithUs.jsx
@@ -2,21 +2,47 @@ import React from "react";
 
 const WorkingWithUs = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="bg-white">
-          <div className="flex flex-wrap -mr-4 -ml-4">
-            {/* <!-- Post 1 --> */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              <div className="bg-white shadow-mds overflow-hidden">
-                <div className="p-6">
-                  <h2 className="text-2xl font-bold mb-4">Working With Us</h2>
-                  <p className="text-gray-700 text-[15px]">
-                    When you work with a company, you want to know what to
-                    expect
-                  </p>
-                </div>
+    <div className="w-full">
+      <div className="jumbotron text-center">
+        <h1 className="text-4xl font-bold">Working With Us</h1>
+      </div>
+      <div className="flex flex-col items-center">
+        {/* Breadcrumb Section */}
+        <div className="w-full md:w-10/12">
+          <div className="flex justify-center">
+            <div className="w-full mb-5">
+              <div className="bread-crumb text-sm text-gray-800 px-[15px] py-2">
+                <a href="" className="text-blue-500">
+                  Home
+                </a>
+                &nbsp;&nbsp;»&nbsp;&nbsp;Working With Us
               </div>
+            </div>
+          </div>
+          <div class="container mx-auto px-4">
+            {/* Page content */}
+            <div className="page-information space-y-6 text-justify mt-10 mb-6">
+              <p className="hidden">
+                Working with us, Working with us in Basingstoke, Working with us
+                in London, Working with us in Near Me, Working with us in UK
+              </p>
+
+              <h3 className="text-2xl md:text-3xl font-semibold text-gray-800">
+                Whether you’re large or small, blue-chip or not-for-profit;
+                we’ll provide you with the same friendly, can-do, responsive,
+                and professional service.
+              </h3>
+
+              <p className="text-gray-600 text-[15px]">
+                When engaging with a company, it's crucial to have clear
+                expectations upfront. Questions about whether they align with
+                your company's needs, deliver on requirements at a reasonable
+                cost and within deadlines, and provide a positive overall
+                experience are paramount. At Geecon, we dedicate ourselves to
+                ensuring that our collaboration is not only the right fit but
+                also a positive and satisfying experience, making it one you'd
+                choose to replicate.
+              </p>
             </div>
           </div>
         </div>
