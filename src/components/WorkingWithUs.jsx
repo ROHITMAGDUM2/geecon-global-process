9fd26d519553802616e233942ac5126892873ab7
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2024-11-21 15:54:37 +0530

    workingn with us 01

--- src/components/WorkingWithUs.jsx
+++ src/components/WorkingWithUs.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const WorkingWithUs = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="bg-white">
+          <div className="flex flex-wrap -mr-4 -ml-4">
+            {/* <!-- Post 1 --> */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="bg-white shadow-mds overflow-hidden">
+                <div className="p-6">
+                  <h2 className="text-2xl font-bold mb-4">Working With Us</h2>
+                  <p className="text-gray-700 text-[15px]">
+                    When you work with a company, you want to know what to
+                    expect
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkingWithUs;
