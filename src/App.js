57c99dd01b2660829f4d9b15ca482bd7c16f8525
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2024-11-21 16:01:55 +0530

    Working With Us 03

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Process from "./components/Process";
+import WorkingWithUs from "./components/WorkingWithUs";
 
 function App() {
   return (
     <>
       <div className="text-center bg-gray-100 min-h-screen flex flex-col items-center font-sans text-white text-[calc(10px+2vmin)]">
-        <Process />
+        {/* <Process /> */}
+        <WorkingWithUs />
       </div>
     </>
   );
