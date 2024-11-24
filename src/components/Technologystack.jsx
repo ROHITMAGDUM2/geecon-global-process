b6ce1c79255dc54aa07615e4c376452365c2503d
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2024-11-24 19:42:56 +0530

    Technology stack DevelopmentFrameworks

--- src/components/Technologystack.jsx
+++ src/components/Technologystack.jsx
@@ -30,6 +30,57 @@ const Technologystack = () => {
             </div>
           </div>
         </div>
+        {/* DevelopmentFrameworks */}
+        <div className="container mx-auto px-4 pb-4">
+          <h3 className="text-gray-700 text-2xl text-center lg:text-left mb-0 mt-[40px] font-light">
+            Development Frameworks
+          </h3>
+          <hr className="border-gray-300 mt-2 mb-10" />
+
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-6 text-center lg:text-left">
+            {[
+              {
+                src: "https://www.one-beyond.com/app/uploads/2018/01/logo_dot_net_framework.svg",
+                alt: "a1 sporting speakers",
+                title: "ASP.NET CORE",
+              },
+              {
+                src: "https://www.one-beyond.com/app/uploads/2018/01/logo_asp_dot_net_core.svg",
+                alt: "silverback studios",
+              },
+              {
+                src: "https://www.one-beyond.com/app/uploads/2018/01/logo_asp_dot_net_webforms.png",
+                alt: "white label software developer",
+              },
+              {
+                src: "https://www.one-beyond.com/app/uploads/2018/01/logo_asp_dot_net_webforms-1.png",
+                alt: "oseasons",
+              },
+              {
+                src: "https://www.one-beyond.com/app/uploads/2018/01/logo_asp_dot_net_winforms.png",
+                alt: "secure client portal software",
+              },
+              {
+                src: "https://www.one-beyond.com/app/uploads/2018/01/logo_asp_dot_net_presentation_windows_foundation.png",
+                alt: "a1 sporting",
+              },
+              {
+                src: "https://www.one-beyond.com/app/uploads/2018/01/logo_xamarin.svg",
+                alt: "rahul international",
+              },
+            ].map((item, index) => (
+              <a key={index} href="#" className="block">
+                <div className="box-tech" data-title={item.title || undefined}>
+                  <img
+                    className="w-full rounded border border-gray-300 shadow-sm"
+                    src={item.src}
+                    alt={item.alt}
+                  />
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
